626808ba92df3621cdb719b781fd68f3efeb437c
caconstan <63742255+caconstan@users.noreply.github.com>
2022-12-30 11:27:56 -0500

    change back to 2.24.1

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aviatrix = {
       source  = "AviatrixSystems/aviatrix"
-      version = "~> 3.0.0"
+      version = "~> 2.24.1"
     }
     aws = {
       source  = "hashicorp/aws"
