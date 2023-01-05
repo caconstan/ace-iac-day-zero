c3219ec1d25766b33f1cce039353938a39a10a61
caconstan <63742255+caconstan@users.noreply.github.com>
2023-01-05 12:03:01 -0500

    Update main.tf
    commented out aviatrix_segmentation_network_domain_connection_policy

--- main.tf
+++ main.tf
@@ -78,8 +78,10 @@ resource "aviatrix_segmentation_network_domain" "BU2" {
     module.aws_transit_1
   ]
 }
-  
+
+/*
 resource "aviatrix_segmentation_network_domain_connection_policy" "BU1_BU2" {
   domain_name_1 = "BU1"
   domain_name_2 = "BU2"
 }
+*/
